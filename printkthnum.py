2a020cd03eb72ec4f63803beb8bfbb65122bc598
christinajoice <christinallrejoice@gmail.com>
2019-03-01 14:22:42 +0530

    Update printkthnum.py

--- printkthnum.py
+++ printkthnum.py
@@ -1,1 +1,8 @@
-n=int(input("Enter 
+n=int(input("Enter no. of values"))
+k=int(input("Enter search values"))
+li=[]
+print("Enter values")
+for i in range(0,n):
+  li.append(input())
+print(li[k-1])
+            
