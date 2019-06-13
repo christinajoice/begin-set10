f328d39b1e5e55b2a71a6d23a8ac48bea26ab8a9
christinajoice <christinallrejoice@gmail.com>
2019-06-13 12:46:35 +0530

    Update naturalchange.py

--- naturalchange.py
+++ naturalchange.py
@@ -1,10 +1,6 @@
-n=int(input("Enter number of natural numbers"))
-li=[]
-for i in range(0,n):
-  li.append(input())
-str=" "
-str.join(li)
-for i in range(0,len(str)):
-  if str[i+1]!=str[i]+1:
+n=int(input())
+li=list(map(int,input().split()))
+for i in range(len(li)):
+  if li[i+1]!=li[i]+1:
     break
-print(i-1)
+print(i+1)
