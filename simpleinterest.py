d8c2ed8be6fa9e3fb4ac85b826b1306e0371847f
christinajoice <christinallrejoice@gmail.com>
2019-06-13 12:37:28 +0530

    Update simpleinterest.py

--- simpleinterest.py
+++ simpleinterest.py
@@ -1,5 +1,3 @@
-p=int(input("Enter principle amount"))
-t=int(input("Enter time in years"))
-r=int(input("Enter rate of interest"))
+p,t,r=map(int,input().split())
 interest=(p*t*r)/100
-print(interest)
+print(int(interest))
